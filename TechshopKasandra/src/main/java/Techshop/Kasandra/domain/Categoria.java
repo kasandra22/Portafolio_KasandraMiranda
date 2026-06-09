70336cc38d7657e2e3fddc169765dd32bc1546e8
kasandra22 <mkasandra709@gmail.com>
2026-06-09 17:55:54 -0600

    Semana 3
    Implementación de base de datos, dependencias.
    Paquete domain creado

---
+++ TechshopKasandra/src/main/java/Techshop/Kasandra/domain/Categoria.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Techshop.Kasandra.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table (name = "categoria")
+/**
+ *
+ * @author HP
+ */
+public class Categoria  implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "id_categoria")
+    private Integer idCategoria;
+    
+    @Column (unique = true, nullable = false, length = 50)
+    @NotNull
+    @Size (max = 50)
+    private String description;
+    
+    @Column (length = 1024)
+    @Size (max = 1024)
+    private String rutaImage;
+    
+    @Column (name = "activo")
+    private boolean activo;
+            
+}
