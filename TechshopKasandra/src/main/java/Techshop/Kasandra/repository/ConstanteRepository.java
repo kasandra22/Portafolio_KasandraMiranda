5eb7f47246c00d7e649162c5579c78e640ad0674
kasandra22 <mkasandra709@gmail.com>
2026-08-08 20:32:09 -0600

    Semana 13
    Se implementa: Domain, Controller, Service, Repository de constante.
    
    Funciona correctamente

---
+++ TechshopKasandra/src/main/java/Techshop/Kasandra/repository/ConstanteRepository.java
@@ -0,0 +1,11 @@
+package Techshop.Kasandra.repository;
+
+import Techshop.Kasandra.domain.Constante;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConstanteRepository extends JpaRepository<Constante, Integer> {
+
+    public Optional<Constante> findByAtributo(String atributo);
+
+}
